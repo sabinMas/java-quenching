dbef831d0ce138fd4b16392b8949590c2e1c0d1e
sabinMas <masonsabin@gmail.com>
2026-09-23 18:48:24 -0700

    biggest number

--- src/Practice.java
+++ src/Practice.java
@@ -87,7 +87,20 @@ public class Practice {
      * @throws IllegalArgumentException if head is null
      */
     public static int biggestNumber(ListNode<Integer> head) {
-        return 0;
+        if (head == null) {
+            throw new IllegalArgumentException("head shouldn't be null")
+
+        }
+        int biggest = head.data;
+        ListNode<Integer> current = head.next;
+
+        while(current!=null){
+            if (current.data>biggest) {
+                biggest = current.data;
+            }
+            current = current.next;
+        }
+        return biggest;
     }
 
     /**
